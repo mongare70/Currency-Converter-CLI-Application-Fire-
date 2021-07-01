60a060885ddc97aa3b2f57af23a17ea270ea08d5
Hillary Mongare <hillarymongare70@gmail.com>
2021-07-01 11:05:58 +0300

    updates

--- app/currency_converter.py
+++ app/currency_converter.py
@@ -1,31 +1,59 @@
 import requests
 import fire
+import json
+
+# set consisting supported currencies
+supported_currencies = {'KES', 'USD', 'EUR', 'JPY', 'GBP', 'CHF', 'CAD', 'AUD', 'ZAR', 'NZD'}
 
 # currency convert class
 class CurrencyConverter:
 
     # constructor
-    def __init__(self, base_currency, result_currency, amount_to_convert):
-        self.base = base_currency
-        self.result = result_currency
-        self.amount = amount_to_convert
+    def __init__(self, base, result, amount):
+        self.base = base
+        self.result = result
+        self.amount = amount
 
 
     # Convert currency method
     def convert(self):
         
-        url = "https://currency-converter5.p.rapidapi.com/currency/convert"
+        # check if base, result, and amount have been parsed
+        if self.base and self.result and self.amount:
 
-        querystring = {"format":"json","from":self.base,"to":self.result,"amount":"{}".format(self.amount)}
+            # check if base currency and result currency are supported by the CLI app
+            if self.base in supported_currencies and self.result in supported_currencies:
 
-        headers = {
-            'x-rapidapi-key': "4c7fdf69eemsh8d6039574576e92p17fed4jsnf84661b04230",
-            'x-rapidapi-host': "currency-converter5.p.rapidapi.com"
-        }
+                # check if amount is integer or float
+                if isinstance(self.amount, int) or isinstance(self.amount, float):
+                    url = "https://currency-converter5.p.rapidapi.com/currency/convert"
 
-        response = requests.request("GET", url, headers=headers, params=querystring)
+                    querystring = {"format":"json","from":self.base,"to":self.result,"amount":"{}".format(self.amount)}
 
-        return response.text
+                    headers = {
+                        'x-rapidapi-key': "4c7fdf69eemsh8d6039574576e92p17fed4jsnf84661b04230",
+                        'x-rapidapi-host': "currency-converter5.p.rapidapi.com"
+                    }
+
+                    response = requests.request("GET", url, headers=headers, params=querystring)
+
+                    response = response.text
+
+                    # convert currency and put it into a dictionary
+                    response = json.loads(response)
+
+                    # return response
+                    return response['rates']
+                
+                else:
+                    return False
+
+            else:
+                return False
+        else:
+            return False
+        
+       
 
 if __name__ == "__main__":
     fire.Fire(CurrencyConverter)
--- app/test_currency_converter.py
+++ app/test_currency_converter.py
@@ -3,49 +3,50 @@ from currency_converter import CurrencyConverter
 
 class TestCurrencyConverter(unittest.TestCase):
     
-    # test with no command 
-    def test_convert_no_command(self):
-        self.assertIs(CurrencyConverter().convert(), '92.6042')
+    # test with no arguments 
+    def test_convert_no_argument(self):
+        self.assertFalse(CurrencyConverter('', '', '').convert())
 
-    # test with single command 
-    def test_convert_single_command(self):
-        self.assertEqual(CurrencyConverter("KES").convert(), '92.6042')
+    # test with only base argument 
+    def test_convert_only_base(self):
+        self.assertFalse(CurrencyConverter('KES', '', '').convert())
 
-    # test with two commands
-    def test_convert_two_command(self):
-        self.assertEqual(CurrencyConverter("KES", "USD").convert(), '92.6042')
+    # test with only result argument
+    def test_convert_only_result(self):
+        self.assertFalse(CurrencyConverter('', 'USD', '').convert())
 
-    # test with all commands
-    def test_convert_all_command(self):
-        self.assertEqual(CurrencyConverter("KES", "USD", 10000).convert(), '92.6042')
+    # test with all arguments
+    def test_convert_all_arguments(self):
+        self.assertAlmostEqual(CurrencyConverter("KES", "USD", 10000).convert(),
+        {'USD': {"currency_name": "United States dollar", "rate": "0.0093", "rate_for_amount": "92.7795"}})
 
-    # test with wrong base currency
-    def test_convert_wrong_base_currency(self):
-        self.assertEqual(CurrencyConverter("HHH", "USD", 10000).convert(), '92.6042')
+    # test with wrong base argument
+    def test_convert_wrong_base(self):
+        self.assertFalse(CurrencyConverter("HHH", "USD", 10000).convert())
 
-    # test with wrong result currency
-    def test_convert_wrong_result_currency(self):
-        self.assertEqual(CurrencyConverter("KES", "HHH", 10000).convert(), '92.6042')
+    # test with wrong result argument
+    def test_convert_wrong_result(self):
+        self.assertFalse(CurrencyConverter("KES", "HHH", 10000).convert())
 
-    # test with wrong amount to convert
-    def test_convert_wrong_amount_to_convert(self):
-        self.assertEqual(CurrencyConverter("KES", "USD", 'HHH').convert(), '92.6042')
+    # test with wrong amount argument
+    def test_convert_wrong_amount(self):
+        self.assertFalse(CurrencyConverter("KES", "USD", 'HHHH').convert())
 
-    # test with wrong base currency & result currency
-    def test_convert_wrong_base_currency_and_result_currency(self):
-        self.assertEqual(CurrencyConverter("HHH", "HHH", 10000).convert(), '92.6042')
+    # test with wrong base argument & result argument
+    def test_convert_wrong_base_and_result(self):
+        self.assertFalse(CurrencyConverter("HHH", "HHH", 10000).convert())
 
-    # test with wrong base currency & amount_to_convert
-    def test_convert_wrong_base_currency_and_amount_to_convert(self):
-        self.assertEqual(CurrencyConverter("HHH", "USD", 'HHH').convert(), '92.6042')
+    # test with wrong base argument & amount argument
+    def test_convert_wrong_base_and_amount(self):
+        self.assertFalse(CurrencyConverter("HHH", "USD", 'HHH').convert())
 
-    # test with wrong base currency & result currency
-    def test_convert_wrong_result_currency_and_amount_to_convert(self):
-        self.assertEqual(CurrencyConverter("KES", "HHH", 'HHH').convert(), '92.6042')
+    # test with wrong base argument & result argument
+    def test_convert_wrong_result_and_amount(self):
+        self.assertFalse(CurrencyConverter("KES", "HHH", 'HHH').convert())
 
-    # test with all wrong inputs
+    # test with all wrong arguments
     def test_convert_wrong_all(self):
-        self.assertEqual(CurrencyConverter("HHH", "HHH", 'HHH').convert(), '92.6042')
+        self.assertFalse(CurrencyConverter("HHH", "HHH", 'HHH').convert())
     
 
 # if __name__ == '__main__':
